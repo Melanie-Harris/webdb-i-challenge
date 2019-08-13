ae78123e13195e465e60ab16e9bf16a612d35519
Melanie Harris <mell.j.harris@gmail.com>
2019-08-12 17:09:25 -0700

    working through account api

---
+++ router.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const db = require('./data/dbConfig.js');
+const router= express.Router();
+
+router.get('/', (req, res)=>{
+
+})
+
+router.get('/:id'), (req, res)=>{
+
+}
+
+
+
+
+module.exports = router;
